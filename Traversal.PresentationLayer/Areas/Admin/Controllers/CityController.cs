6718c9925c94b4effbabce9948b88935745a94db
Ali Kaan Yayla <naklovcuas@gmail.com>
2023-06-26 11:55:55 +0300

    ajax operations

--- Traversal.PresentationLayer/Areas/Admin/Controllers/CityController.cs
+++ Traversal.PresentationLayer/Areas/Admin/Controllers/CityController.cs
@@ -2,43 +2,83 @@
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Collections.Generic;
+using Traversal.BusinessLayer.Abstract;
+using Traversal.EntityLayer.Concrate;
 using Traversal.PresentationLayer.Models;
 
 namespace Traversal.PresentationLayer.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CityController : Controller
     {
-        [Area("Admin")]
+        private readonly IDestinationService _DestinationService;
+        public CityController(IDestinationService destinationService)
+        {
+            _DestinationService = destinationService;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
         public IActionResult CityList()
         {
-            var jsonCity = JsonConvert.SerializeObject(cities);
+            var jsonCity = JsonConvert.SerializeObject(_DestinationService.TGetList());
             return Json(jsonCity);
         }
 
-        public static List<CityClass> cities = new List<CityClass>
+        [HttpPost]
+        public IActionResult AddCityDestination(Destination destination)
         {
-            new CityClass
-            {
-                CityID=1,
-                CityName="Üsküp",
-                CityCountry="Makedonya"
-            },
-            new CityClass
-            {
-                CityID=2,
-                CityName="Roma",
-                CityCountry="İtalya"
-            },
-            new CityClass
-            {
-                CityID=3,
-                CityName="Londra",
-                CityCountry="İngiltere"
-            }
-        };
+            destination.Status = true;
+            _DestinationService.TAdd(destination);
+            var values = JsonConvert.SerializeObject(destination);
+            return Json(values);
+        }
+
+        public IActionResult GetById(int DestinationID)
+        {
+            var values = _DestinationService.TGetById(DestinationID);
+            var jsonValues = JsonConvert.SerializeObject(values);
+            return Json(jsonValues);
+        }
+
+        public IActionResult DeleteCity(int id)
+        {
+            var values = _DestinationService.TGetById(id);
+            _DestinationService.TDelete(values);
+            return NoContent();
+        }
+
+        public IActionResult UpdateCity(Destination destination)
+        {
+            _DestinationService.TUpdate(destination);
+            var v = JsonConvert.SerializeObject(destination);
+            return Json(v);
+        }
+
+        //public static List<CityClass> cities = new List<CityClass>
+        //{
+        //    new CityClass
+        //    {
+        //        CityID=1,
+        //        CityName="Üsküp",
+        //        CityCountry="Makedonya"
+        //    },
+        //    new CityClass
+        //    {
+        //        CityID=2,
+        //        CityName="Roma",
+        //        CityCountry="İtalya"
+        //    },
+        //    new CityClass
+        //    {
+        //        CityID=3,
+        //        CityName="Londra",
+        //        CityCountry="İngiltere"
+        //    }
+        //};
+
+
     }
 }
