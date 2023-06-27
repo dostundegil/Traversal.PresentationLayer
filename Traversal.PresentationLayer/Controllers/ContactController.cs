c24e89ad256de8c280f751eed0ff505241cffdf1
Ali Kaan Yayla <naklovcuas@gmail.com>
2023-06-28 00:14:56 +0300

    guide page added

---
+++ Traversal.PresentationLayer/Controllers/ContactController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Traversal.BusinessLayer.Abstract;
+
+namespace Traversal.PresentationLayer.Controllers
+{
+    [AllowAnonymous]
+    public class ContactController : Controller
+    {
+        private readonly IContactService _contactService;
+
+        public ContactController(IContactService contactService)
+        {
+            _contactService = contactService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Traversal.PresentationLayer/Controllers/GuideController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Traversal.BusinessLayer.Abstract;
+
+namespace Traversal.PresentationLayer.Controllers
+{
+    [AllowAnonymous]
+    public class GuideController : Controller
+    {
+        private readonly IGuideService _guideService;
+
+        public GuideController(IGuideService guideService)
+        {
+            _guideService = guideService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _guideService.TGetList();
+            return View(values);
+        }
+    }
+}
