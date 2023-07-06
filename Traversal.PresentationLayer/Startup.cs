e3e39ddbf8aea14b0097008270c159b5276d219c
Ali Kaan YAYLA <kaan_sv@hotmail.com>
2023-07-06 20:36:48 +0300

    route config updated

--- Traversal.PresentationLayer/Startup.cs
+++ Traversal.PresentationLayer/Startup.cs
@@ -119,13 +119,13 @@ namespace Traversal.PresentationLayer
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Default}/{action=Index}/{id?}");
             });
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                   name: "areas",
-                  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                  pattern: "{area:exists}/{controller=Default}/{action=Index}/{id?}"
                 );
             });
 
@@ -133,7 +133,7 @@ namespace Traversal.PresentationLayer
             {
                 endpoints.MapControllerRoute(
                   name: "areas",
-                  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                  pattern: "{area:exists}/{controller=Default}/{action=Index}/{id?}"
                 );
             });
         }
